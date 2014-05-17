778d541bb82e5b054c87823a372c355ca95dbac6
Nima Talebi <nima.talebi@sirca.org.au>
2014-05-17 19:50:40 +1000

    Stack push and pop basic fixes and unit tests

--- unit-test.rb
+++ unit-test.rb
@@ -4,6 +4,8 @@ require_relative 'memory'
 
 require 'minitest/autorun'
 
+#. Tests -={
+#. Registers -={
 class TestMath < Minitest::Test
   def setup
     @cpu = Processor.new
@@ -93,3 +95,59 @@ class TestMath < Minitest::Test
     assert_equal 1,@cpu.edx.bits
   end
 end
+#. }=-
+#. Memory -={
+class TestMem < Minitest::Test
+  def setup
+    @cpu = Processor.new
+    @mem = @cpu.mem
+  end
+
+  def test_mem
+    addr = 0x0400f000
+    @mem.set(addr, "\xAA\xBB\xCC\xDD\x11\x00\x44\x33", 8)
+    assert_equal "\xAA\xBB\xCC\xDD", @mem.get(addr, 4)
+
+    addr = 0x0400e000
+    @mem.set(addr, 0x41424344, 4)
+    assert_equal "DCBA", @mem.get(addr, 4)
+  end
+end
+#. }=-
+#. Stack -=
+class TestStack < Minitest::Test
+  def setup
+    @cpu = Processor.new
+    @stack = @cpu.stack
+  end
+
+  def test_stack_pointer
+    assert_equal 0xFFFF0000, @stack.base.addr
+    assert_equal 0xFFFF0000, @stack.stack.addr
+
+    @stack.push  0x51525354
+    assert_equal 0xFFFF0000, @stack.base.addr
+    assert_equal 0xFFFEFFFC, @stack.stack.addr
+
+      @stack.push  0x61626364
+      assert_equal 0xFFFEFFF8, @stack.stack.addr
+
+        @stack.push  "\x41\x42\x43\x44"
+        assert_equal 0xFFFEFFF4, @stack.stack.addr
+
+    assert_equal 0xFFFF0000, @stack.base.addr
+
+        assert_equal "\x41\x42\x43\x44", @stack.pop
+        assert_equal 0xFFFEFFF8, @stack.stack.addr
+
+      assert_equal "\x64\x63\x62\x61", @stack.pop
+      assert_equal 0xFFFEFFFC, @stack.stack.addr
+
+    assert_equal "\x54\x53\x52\x51", @stack.pop
+    assert_equal 0xFFFF0000, @stack.stack.addr
+
+    assert_equal 0xFFFF0000, @stack.base.addr
+  end
+end
+#. }=-
+#. }=-
