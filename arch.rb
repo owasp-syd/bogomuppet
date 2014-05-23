c8f8033e54779f1fa7ae78d0d9243b7c6cb6c007
Nima Talebi <nima.talebi@sirca.org.au>
2014-05-23 22:30:51 +1000

    Added xmmword, ymmword, and some more comments

--- arch.rb
+++ arch.rb
@@ -2,16 +2,18 @@ class Architecture
   attr_reader :bits, :bytes, :mask
 
   @@tokens = {
-    bit:     0x01,
-    nybble:  0x04, #. half byte per nybble
-    byte:    0x08, #. 8 bits per byte
-    word:    0x10, #. 2 bytes per word  (16-bit)
-    dword:   0x20, #. 4 bytes per dword (32-bit)
-    qword:   0x40, #. 8 bytes per qword (64-bit)
-    BYTE:    0x01, #. 1 byte per byte
-    WORD:    0x02, #. 2 bytes per word  (16-bit)
-    DWORD:   0x04, #. 4 bytes per dword (32-bit)
-    QWORD:   0x08, #. 8 bytes per qword (64-bit)
+    bit:      0x01, #. binary digit               / bit
+    nybble:   0x04, #. half byte per nybble       /
+    byte:     0x08, #.  8 bits per byte            /
+    word:     0x10, #.  2 bytes per word  (16-bit) / short int
+    dword:    0x20, #.  4 bytes per dword (32-bit) /       int
+    qword:    0x40, #.  8 bytes per qword (64-bit) / long long
+    xmmword:  0x80, #. 16 bytes per xmmword SSE XMM registers, paragraph
+    ymmword: 0x100, #. 32 bytes per ymmword SSE XMM registers, double paragraph
+    BYTE:     0x01, #.  1 bytes per byte           /
+    WORD:     0x02, #.  2 bytes per word  (16-bit) /
+    DWORD:    0x04, #.  4 bytes per dword (32-bit) /
+    QWORD:    0x08, #.  8 bytes per qword (64-bit) /
   }
 
   def self.[](key) @@tokens[key] end
