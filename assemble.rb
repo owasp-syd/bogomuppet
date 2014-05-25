e88016c5e4162643ddb19e97a2e7074532707140
Nima Talebi <nima.talebi@sirca.org.au>
2014-05-25 19:23:44 +1000

    Dijested the jestful joker

--- assemble.rb
+++ assemble.rb
@@ -4,27 +4,27 @@ require_relative 'processor'
 
 require 'parslet'
 class Assembler < Parslet::Parser
-  rule(:eol)         { str("\n") | any.absent? }
-  rule(:ws)          { match('[\s\t]').repeat(1) }
-  rule(:delim)       { ws.maybe >> str(',') >> ws.maybe }
-  rule(:comment)     { str(";") >> any.repeat }
+  rule(:eol)     { str("\n") | any.absent? }
+  rule(:ws)      { match('[\s\t]').repeat(1) }
+  rule(:delim)   { ws.maybe >> str(',') >> ws.maybe }
+  rule(:comment) { str(";") >> any.repeat }
 
-  rule(:regseg)      {
+  rule(:regseg) {
     (
       str('cs') | str('fs') | str('gs') |
       str('ds') | str('ss') | str('es')
     ).as(:regseg) >> str(':')
   }
-  rule(:regb)         {
-    str('ah')  | str('bh')  | str('ch')  | str('dh')  |
-    str('al')  | str('bl')  | str('cl')  | str('dl')
+  rule(:regb) {
+    str('ah') | str('bh') | str('ch') | str('dh') |
+    str('al') | str('bl') | str('cl') | str('dl')
   }
-  rule(:regw)         {
-    str('ax')  | str('bx')  | str('cx')  | str('dx')  |
-      str('cs') | str('fs') | str('gs') |
-      str('ds') | str('ss') | str('es')
+  rule(:regw) {
+    str('ax') | str('bx') | str('cx') | str('dx') |
+    str('cs') | str('fs') | str('gs') |
+    str('ds') | str('ss') | str('es')
   }
-  rule(:regd)         {
+  rule(:regd) {
     str('eax') | str('ebx') | str('ecx') | str('edx') |
     str('edi') | str('esi') | str('ebp') | str('esp')
   }
@@ -37,60 +37,60 @@ class Assembler < Parslet::Parser
     regseg.maybe >>
     str('[') >> (
       (
-        reg.as(:reg)
+        regd.as(:reg)
       ) >> (
         opmath.as(:op) >> ( reg.as(:reg) | imm.as(:imm) | decimal.as(:int) )
       ).repeat
     ) >> str(']')
   }
 
-  rule(:memb)        { str('BYTE') >> ws >> str('PTR') >> ws >> ptrdrf.as(:ptr) }
-  rule(:memw)        { str('WORD') >> ws >> str('PTR') >> ws >> ptrdrf.as(:ptr) }
-  rule(:memd)        { str('DWORD') >> ws >> str('PTR') >> ws >> ptrdrf.as(:ptr) }
-  rule(:mem)         { memb | memw | memd }
+  rule(:memb) { str('BYTE')  >> ws >> str('PTR') >> ws >> ptrdrf.as(:ptr) }
+  rule(:memw) { str('WORD')  >> ws >> str('PTR') >> ws >> ptrdrf.as(:ptr) }
+  rule(:memd) { str('DWORD') >> ws >> str('PTR') >> ws >> ptrdrf.as(:ptr) }
+  rule(:mem)  { memb | memw | memd }
 
   #. <op> <reg>|<mem>, <reg>|<imm> -={
-  rule(:opdstAb)      { regb.as(:reg) | memb.as(:mem) }
-  rule(:opsrcAb)      { regb.as(:reg) | imm.as(:imm) }
+  rule(:opdstAb) { regb.as(:reg) | memb.as(:mem) }
+  rule(:opsrcAb) { regb.as(:reg) | imm.as(:imm) }
 
-  rule(:opdstAw)      { regw.as(:reg) | memw.as(:mem) }
-  rule(:opsrcAw)      { regw.as(:reg) | imm.as(:imm) }
+  rule(:opdstAw) { regw.as(:reg) | memw.as(:mem) }
+  rule(:opsrcAw) { regw.as(:reg) | imm.as(:imm) }
 
-  rule(:opdstAd)      { regd.as(:reg) | memd.as(:mem) }
-  rule(:opsrcAd)      { regd.as(:reg) | imm.as(:imm) }
+  rule(:opdstAd) { regd.as(:reg) | memd.as(:mem) }
+  rule(:opsrcAd) { regd.as(:reg) | imm.as(:imm) }
   #. }=-
 
   #. <op> <reg>, <reg>|<mem>|<imm> -={
-  rule(:opdstBb)      { regb.as(:reg) }
-  rule(:opsrcBb)      { regb.as(:reg) | memb.as(:mem) | imm.as(:imm) }
+  rule(:opdstBb) { regb.as(:reg) }
+  rule(:opsrcBb) { regb.as(:reg) | memb.as(:mem) | imm.as(:imm) }
 
-  rule(:opdstBw)      { regw.as(:reg) }
-  rule(:opsrcBw)      { regw.as(:reg) | memw.as(:mem) | imm.as(:imm) }
+  rule(:opdstBw) { regw.as(:reg) }
+  rule(:opsrcBw) { regw.as(:reg) | memw.as(:mem) | imm.as(:imm) }
 
-  rule(:opdstBd)      { regd.as(:reg) }
-  rule(:opsrcBd)      { regd.as(:reg) | memd.as(:mem) | imm.as(:imm) }
+  rule(:opdstBd) { regd.as(:reg) }
+  rule(:opsrcBd) { regd.as(:reg) | memd.as(:mem) | imm.as(:imm) }
   #. }=-
 
-  rule(:mov)         {
-    str('mov').as(:opcode) >> ws >> opdstAb.as(:opdst) >> delim >> ( opsrcAb ).as(:opsrc) |
-    str('mov').as(:opcode) >> ws >> opdstAw.as(:opdst) >> delim >> ( opsrcAw ).as(:opsrc) |
-    str('mov').as(:opcode) >> ws >> opdstAd.as(:opdst) >> delim >> ( opsrcAd ).as(:opsrc) |
-    str('mov').as(:opcode) >> ws >> opdstBb.as(:opdst) >> delim >> ( opsrcBb ).as(:opsrc) |
-    str('mov').as(:opcode) >> ws >> opdstBw.as(:opdst) >> delim >> ( opsrcBw ).as(:opsrc) |
-    str('mov').as(:opcode) >> ws >> opdstBd.as(:opdst) >> delim >> ( opsrcBd ).as(:opsrc)
+  rule(:mov) {
+    str('mov').as(:opcode) >> ws >> opdstAd.as(:opdst) >> delim >> opsrcAd.as(:opsrc) |
+    str('mov').as(:opcode) >> ws >> opdstBd.as(:opdst) >> delim >> opsrcBd.as(:opsrc) |
+    str('mov').as(:opcode) >> ws >> opdstAw.as(:opdst) >> delim >> opsrcAw.as(:opsrc) |
+    str('mov').as(:opcode) >> ws >> opdstBw.as(:opdst) >> delim >> opsrcBw.as(:opsrc) |
+    str('mov').as(:opcode) >> ws >> opdstAb.as(:opdst) >> delim >> opsrcAb.as(:opsrc) |
+    str('mov').as(:opcode) >> ws >> opdstBb.as(:opdst) >> delim >> opsrcBb.as(:opsrc)
   }
-  rule(:add)         {
-    str('add').as(:opcode) >> ws >> opdstAb.as(:opdst) >> delim >> ( opsrcAb ).as(:opsrc) |
-    str('add').as(:opcode) >> ws >> opdstAw.as(:opdst) >> delim >> ( opsrcAw ).as(:opsrc) |
-    str('add').as(:opcode) >> ws >> opdstAd.as(:opdst) >> delim >> ( opsrcAd ).as(:opsrc) |
-    str('add').as(:opcode) >> ws >> opdstBb.as(:opdst) >> delim >> ( opsrcBb ).as(:opsrc) |
-    str('add').as(:opcode) >> ws >> opdstBw.as(:opdst) >> delim >> ( opsrcBw ).as(:opsrc) |
-    str('add').as(:opcode) >> ws >> opdstBd.as(:opdst) >> delim >> ( opsrcBd ).as(:opsrc)
+  rule(:add) {
+    str('add').as(:opcode) >> ws >> opdstAd.as(:opdst) >> delim >> opsrcAd.as(:opsrc) |
+    str('add').as(:opcode) >> ws >> opdstBd.as(:opdst) >> delim >> opsrcBd.as(:opsrc) |
+    str('add').as(:opcode) >> ws >> opdstAw.as(:opdst) >> delim >> opsrcAw.as(:opsrc) |
+    str('add').as(:opcode) >> ws >> opdstBw.as(:opdst) >> delim >> opsrcBw.as(:opsrc) |
+    str('add').as(:opcode) >> ws >> opdstAb.as(:opdst) >> delim >> opsrcAb.as(:opsrc) |
+    str('add').as(:opcode) >> ws >> opdstBb.as(:opdst) >> delim >> opsrcBb.as(:opsrc)
   }
 
   rule(:expr)        { mov | add }
   rule(:line)        { expr.maybe >> comment.maybe >> eol }
-  root :line
+  root(:line)
 end
 
 def get_instructions(inst)
