9662d232785cf118e25d909f327b9cb1edc1880b
Nima Talebi <me@nima.id.au>
2014-05-27 18:06:43 +1000

    POC libdisasm code in ruby

--- asm/00-c-in-ruby.rb
+++ asm/00-c-in-ruby.rb
@@ -1,5 +1,14 @@
+#!/usr/bin/env ruby
+require 'inline'
+
 class Cee
   inline(:C) do |builder|
+    builder.include '<stdlib.h>'
+    builder.include '<string.h>'
+    builder.include '<libdis.h>'
+
+    builder.add_compile_flags '-ldisasm'
+
     builder.prefix <<-eos
       /* lang:c */
       #define BIT(n)               ( 1   <<    (n))
@@ -7,6 +16,9 @@ class Cee
       #define BIT_CLR(bf, mask)    ((bf) & ~(mask))
       #define BIT_FLP(bf, mask)    ((bf) ^  (mask))
       #define BIT_MSK(size)        (BIT(size) - 1 )
+
+      #define BUF_SIZE 1024
+      #define LINE_SIZE 512
       /* lang:c */
     eos
     builder.c <<-eos
@@ -38,12 +50,37 @@ class Cee
       /* lang:c */
     eos
 
+    builder.c <<-eos, method_name: 'disasm', arity: 1
+      /* lang:c */
+      VALUE disasm(VALUE buf) {
+        VALUE res;
+        x86_init(opt_none, NULL, stderr);
+        x86_insn_t insn;
+        long arr1_len = RARRAY_LEN(buf);
+        VALUE *c_arr1 = RARRAY_PTR(buf);
+        int i;
+        unsigned int size;
+        unsigned int pos = 0;
+        unsigned char line[LINE_SIZE];
+        for(i=0; i<arr1_len; i++) {
+          unsigned char *c = StringValueCStr(c_arr1[i]);
+          size = x86_disasm(c, BUF_SIZE, 0, pos, &insn);
+          if(size) {
+            //print instruction
+            x86_format_insn(&insn, &line[0], LINE_SIZE, intel_syntax);
+          }
+        }
+        x86_cleanup();
+        return rb_str_new(line, strlen(line));
+      }
+      /* lang:c */
+    eos
   end
+
   def initialize
     @bits = 0x0
   end
-
-  def bits(mask)
-    return bit_mask(@bits, mask)
-  end
 end
+
+c = Cee.new
+p c.disasm(["\x55"])
